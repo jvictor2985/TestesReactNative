4dcfdd421fbc8f51b5362e65116dc41127659753
jvictor2985 <jvictor2985@gmail.com>
2018-10-05 13:19:21 -0300

    Alguns testes utilizando o React Native

---
+++ Teste/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Container, Header, Content, ListItem, Text, Radio, Right, Left } from 'native-base';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Testando novas views</Text>
+      </View>
+    );
+  }
+}
+
+class RadioButtonExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <ListItem>
+            <Left>
+              <Text>Daily Stand Up</Text>
+            </Left>
+            <Right>
+              <Radio selected={false} />
+            </Right>
+          </ListItem>
+          <ListItem>
+            <Left>
+              <Text>Discussion with Client</Text>
+            </Left>
+            <Right>
+              <Radio selected={true} />
+            </Right>
+          </ListItem>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
